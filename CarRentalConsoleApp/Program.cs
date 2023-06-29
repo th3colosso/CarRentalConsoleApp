0f3d87439f1902bc18463eb13ff047a945bf1a82
Matheus Correa <mattheuuz@outlook.com>
2023-06-29 11:06:34 -0300

    Updated program function Main to interact with the user

--- CarRentalConsoleApp/Program.cs
+++ CarRentalConsoleApp/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace CarRentalConsoleApp {
+﻿using CarRentalConsoleApp.Entities;
+using System.Globalization;
+
+namespace CarRentalConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            
+            Console.WriteLine("Enter rental data:");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup date (dd/MM/yyyy HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return date (dd/MM/yyyy HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
