f8224bab8abd9139af8f72d25b61f3f7dc64b7d2
Matheus Correa <mattheuuz@outlook.com>
2023-06-29 11:19:42 -0300

    Updated function ProcessInvoice

--- CarRentalConsoleApp/Entities/Services/BrazilTaxService.cs
+++ CarRentalConsoleApp/Entities/Services/BrazilTaxService.cs
@@ -1,7 +1,7 @@
 ﻿namespace CarRentalConsoleApp.Entities.Services {
     internal class BrazilTaxService {
         public double Tax(double amount) { 
-            if (amount > 100.0) {
+            if (amount < 100.0) {
                 return (amount * 0.20);
             } else {
                 return (amount * 0.15);
--- CarRentalConsoleApp/Entities/Services/RentalService.cs
+++ CarRentalConsoleApp/Entities/Services/RentalService.cs
@@ -2,6 +2,7 @@
     internal class RentalService {
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
 
         public RentalService(double pricePerHour, double pricePerDay) {
             PricePerHour = pricePerHour;
@@ -9,7 +10,17 @@
         }
 
         public void ProcessInvoice(CarRental carRental) {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+            if (duration.TotalHours <=12.0 ) {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);        
+            } else {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
 
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
