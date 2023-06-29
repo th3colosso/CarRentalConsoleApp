f51d8e350a7a7d0435e31401239872fb4a96f9c8
Matheus Correa <mattheuuz@outlook.com>
2023-06-29 11:12:31 -0300

    Adding services' classes

---
+++ CarRentalConsoleApp/Entities/Services/RentalService.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalConsoleApp.Entities.Services {
+    internal class RentalService {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay) {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental) {
+
+        }
+    }
+}
